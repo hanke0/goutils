cf3238abe3d603957b4de1a90c5d8b1df0d6077f
kehan <kehan@tencent.com>
2022-02-15 16:38:02 +0800

    Better asset error message

--- assert/assert.go
+++ assert/assert.go
@@ -1,7 +1,11 @@
 // package assert provide function useful for unit testing.
 package assert
 
-import "reflect"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
 
 type tHelper interface {
 	Helper()
@@ -11,12 +15,23 @@ type T interface {
 	Errorf(format string, o ...interface{})
 }
 
+func tostring(o interface{}) string {
+	if e, ok := o.(error); ok {
+		return fmt.Sprintf("%v", e)
+	}
+	data, err := json.Marshal(o)
+	if err != nil {
+		return fmt.Sprintf("%#v", o)
+	}
+	return string(data)
+}
+
 func Equal(t T, got interface{}, want interface{}) bool {
 	if !reflect.DeepEqual(got, want) {
 		if h, ok := t.(tHelper); ok {
 			h.Helper()
 		}
-		t.Errorf("\ngot:%#v\nwant:%#v", got, want)
+		t.Errorf("\ngot:%s\nwant:%s", tostring(got), tostring(want))
 		return false
 	}
 	return true
@@ -27,7 +42,7 @@ func NotEqual(t T, got interface{}, want interface{}) bool {
 		if h, ok := t.(tHelper); ok {
 			h.Helper()
 		}
-		t.Errorf("\nshould not be: %#v\n", got)
+		t.Errorf("\nshould not be: %s\n", tostring(got))
 		return false
 	}
 	return true
@@ -40,8 +55,18 @@ func Nil(t T, got interface{}) bool {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
 	}
-	t.Errorf("expect nil but got %#v", got)
+	t.Errorf("expect nil but got %s", tostring(got))
 	return false
+}
+
+func WantError(t T, got error, want bool) bool {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+	if want {
+		return NotNil(t, got)
+	}
+	return Nil(t, got)
 }
 
 func NotNil(t T, got interface{}) bool {
