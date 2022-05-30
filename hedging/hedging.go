5c0d76d38301c9d6ae4962d8419fa1ba34d2a96a
kehan <kehan@tencent.com>
2022-05-30 11:55:46 +0800

    hedging: add function hedging do

---
+++ hedging/hedging.go
@@ -0,0 +1,103 @@
+// Package hedging provides a method to get an no-error answer from two functions.
+// It can describes as is, when A function fails or executes timeout, run B to replace it.
+package hedging // import "github.com/ko-han/goutils/hedging"
+
+import (
+	"sync"
+	"time"
+)
+
+type result struct {
+	data interface{}
+	err  error
+}
+
+// Do performs a hedging calls from primary and fallback.
+//
+// Like
+func Do(hedgingAfter time.Duration, primary, fallback func() (interface{}, error), opt ...Option) (interface{}, error) {
+	if hedgingAfter <= 0 {
+		// calls step by step when no hedging start duration.
+		if o, err := primary(); err != nil {
+			return o, err
+		}
+		return fallback()
+	}
+
+	var state = struct {
+		mu              sync.Mutex
+		fallbackStarted bool
+		primaryDone     chan result
+		fallbackDone    chan result
+		primaryResult   *result
+		fallbackResult  *result
+		opt             option
+	}{
+		primaryDone:  make(chan result, 1),
+		fallbackDone: make(chan result, 1),
+	}
+	for _, a := range opt {
+		a.apply(&state.opt)
+	}
+
+	go func() {
+		var r result
+		r.data, r.err = primary()
+		state.primaryDone <- r
+	}()
+
+	startFallback := func() {
+		state.mu.Lock()
+		if state.fallbackStarted {
+			state.mu.Unlock()
+			return
+		}
+		state.fallbackStarted = true
+		state.mu.Unlock()
+
+		var r result
+		r.data, r.err = fallback()
+		state.fallbackDone <- r
+	}
+	var t *time.Timer
+	if state.opt.after != nil {
+		t = state.opt.after(hedgingAfter, startFallback)
+	} else {
+		t = time.AfterFunc(hedgingAfter, startFallback)
+	}
+	defer t.Stop()
+
+	for {
+		select {
+		case r := <-state.primaryDone:
+			if r.err == nil {
+				return r.data, nil
+			}
+			state.primaryResult = &r
+			if state.fallbackResult != nil {
+				// fallback must fails here.
+				// primary and fallback fails, return primary error.
+				return r.data, r.err
+			}
+			startFallback()
+		case r := <-state.fallbackDone:
+			if r.err == nil {
+				return r.data, nil
+			}
+			state.fallbackResult = &r
+			if state.primaryResult != nil {
+				// primary must fails here, return it error.
+				return state.primaryResult.data, state.primaryResult.err
+			}
+		}
+	}
+}
+
+type option struct {
+	after func(d time.Duration, f func()) *time.Timer
+}
+
+// Option for do hedging job.
+type Option interface {
+	apply(*option)
+}
---
+++ hedging/hedging_test.go
@@ -0,0 +1,47 @@
+package hedging_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ko-han/goutils/hedging"
+)
+
+func ExampleDo() {
+	a := func() (interface{}, error) {
+		time.Sleep(time.Millisecond * 5)
+		return 1, nil
+	}
+	b := func() (interface{}, error) {
+		time.Sleep(time.Millisecond)
+		return 2, nil
+	}
+	fmt.Println(hedging.Do(time.Millisecond, a, b))
+}
+
+func testSleepDo(t *testing.T, t1, t2, t3 time.Duration, want int) {
+	a := func() (interface{}, error) {
+		time.Sleep(t1)
+		return 1, nil
+	}
+	b := func() (interface{}, error) {
+		time.Sleep(t2)
+		return 2, nil
+	}
+	ret, err := hedging.Do(t3, a, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ret != want {
+		t.Fatalf("expect %d got %d", want, ret)
+	}
+}
+
+func TestDoFallback(t *testing.T) {
+	testSleepDo(t, time.Millisecond*10, time.Millisecond, time.Millisecond, 2)
+}
+
+func TestDoPrimary(t *testing.T) {
+	testSleepDo(t, time.Millisecond, time.Millisecond, time.Millisecond, 1)
+}
