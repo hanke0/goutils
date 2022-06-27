faa96d51b217674cbde6ba769d8baee6fe16ddf6
kehan <kehan@tencent.com>
2022-06-27 11:01:32 +0800

    New strings

--- strings/strings.go
+++ strings/strings.go
@@ -1,23 +1,47 @@
 // Package strings contains a bucket of operations of string.
-package strings
+package strings // import "github.com/ko-han/goutils/strings"
 
 import (
 	"strings"
 )
 
-// ListContains checks if sub string is located in a list of strings joined by sep.
+// Contains checks if a string exists in the string slice.
+func Contains(bucket []string, want string) bool {
+	return Index(bucket, want) != -1
+}
+
+// Index get the first index of string slice that value equals to want, or -1
+// if not any value equals to `want`.
+func Index(bucket []string, want string) int {
+	for i, v := range bucket {
+		if v == want {
+			return i
+		}
+	}
+	return -1
+}
+
+// Count counts the number of values that equals to want in the string slice.
+func Count(bucket []string, want string) (num int) {
+	for _, v := range bucket {
+		if v == want {
+			num++
+		}
+	}
+	return
+}
+
+// HasSeparatedSubstring checks if sub string is located in a list of strings joined by sep.
 //
 // Example:
-//     ListContains("10,12,14", "10", ',') => true
-//     ListContains("10,12,14", "1", ',') => false
-//     ListContains("1", "1", ',') => true
-//     ListContains("10,12,14", "1", ',') => false
+//     HasSeparatedSubstring("10,12,14", "10", ',')  => true
+//     HasSeparatedSubstring("10,12,14", "1", ',')   => false
+//     HasSeparatedSubstring("1", "1", ',')          => true
+//     HasSeparatedSubstring("10,12,14,", "14", ',') => false
 //
-// Note that a empty list will always return false.
-func ListContains(list, sub string, sep byte) bool {
-	if list == "" {
-		return false
-	}
+// It handles non-ascii characters not every well.
+// It basic equals to Contains(strings.Split(s, sep), sub), but faster.
+func HasSeparatedSubstring(list, sub string, sep byte) bool {
 	idx := strings.Index(list, sub)
 	if idx < 0 {
 		return false
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -1,8 +1,43 @@
-package strings
+package strings_test
 
 import (
 	"testing"
+
+	"github.com/ko-han/goutils/strings"
 )
+
+func TestContains(t *testing.T) {
+	var list = []string{"1", "2", ""}
+	if !strings.Contains(list, "1") {
+		t.Fatal("1 should in the list")
+	}
+	if strings.Contains(list, "3") {
+		t.Fatal("3 should not in the list")
+	}
+}
+
+func TestIndex(t *testing.T) {
+	var list = []string{"1", "2", ""}
+	if i := strings.Index(list, "1"); i != 0 {
+		t.Fatal("1 should in the list", i)
+	}
+	if i := strings.Index(list, "3"); i != -1 {
+		t.Fatal("3 should not in the list", i)
+	}
+}
+
+func TestCount(t *testing.T) {
+	var list = []string{"1", "2", "3", "1"}
+	if i := strings.Count(list, "1"); i != 2 {
+		t.Fatal("1 should present 2 times", i)
+	}
+	if i := strings.Count(list, "3"); i != 1 {
+		t.Fatal("3 should present 1 times", i)
+	}
+	if i := strings.Count(list, "4"); i != 0 {
+		t.Fatal("4 should present 0 times", i)
+	}
+}
 
 func TestListContains(t *testing.T) {
 	var cases = []struct {
@@ -26,10 +61,12 @@ func TestListContains(t *testing.T) {
 		{",124,", "24", false},
 		{",124,", "134", false},
 		{"", "124", false},
+		{"", "", true},
+		{"123", "", false},
 	}
 
 	for _, c := range cases {
-		if got := ListContains(c.list, c.sub, ','); got != c.want {
+		if got := strings.HasSeparatedSubstring(c.list, c.sub, ','); got != c.want {
 			t.Errorf("list=%s, sub=%s, want=%v, got=%v", c.list, c.sub, c.want, got)
 		}
 	}
